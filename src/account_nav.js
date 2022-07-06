837c79957521ac9ebe35bdd5e8e195cc446a94da
Patrick Marks <pmarks1914@gmail.com>
2022-07-06 13:42:38 +0000

    left sidebar

--- src/account_nav.js
+++ src/account_nav.js
@@ -9,6 +9,10 @@ import {
   cilNotes,
   cilPencil,
   cilPuzzle,
+  cilMoon,
+  cilPeople,
+  cilUser,
+  cilSpeech,
   cilSpeedometer,
   cilStar,
 } from '@coreui/icons'
@@ -20,15 +24,66 @@ const account_nav = [
     name: 'Dashboard',
     to: '/dashboard',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
+    // badge: {
+    //   color: 'info',
+    //   text: 'NEW',
+    // },
   },
-  // {
-  //   component: CNavTitle,
-  //   name: 'Theme',
-  // },
+  {
+    component: CNavGroup,
+    name: 'PAYMENTS',
+    items: [
+      {
+        component: CNavItem,
+        name: 'Transaction',
+        to: '/payment/transaction',
+        icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Customers',
+        to: '/payment/customers',
+        icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Refunds',
+        to: '/payment/refund',
+        icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Payouts',
+        to: '/payment/payout',
+        icon: <CIcon icon={cilMoon} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Payouts',
+        to: '/payment/payout',
+        icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Subaccounts',
+        to: '/payment/subaccount',
+        icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
+      },
+      {
+        component: CNavItem,
+        name: 'Dispute',
+        to: '/payment/dispute',
+        icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
+      },
+    ],
+  },
+  {
+    component: CNavItem,
+    name: 'Charts',
+    to: '/charts',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+  
 ]
 
 export default account_nav
